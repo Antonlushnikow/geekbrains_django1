1a33b29899d3cc0fb0cb31bb5b8ff23f22689439
Anton Lushnikov <antonlushnikow@gmail.com>
2021-06-26 12:10:00 +0500

    lesson2

--- geekshop/geekshop/views.py
+++ geekshop/geekshop/views.py
@@ -1,9 +1,24 @@
+import json
 from django.shortcuts import render
 
 
+with open('geekshop/templates/geekshop/links_menu.json', 'r', encoding="utf-8") as content:
+    links_menu = json.load(content)
+
+
 def index(request):
-    return render(request, 'index.html')
+    title = 'магазин'
+    context = {
+        'title': title,
+        'links_menu': links_menu
+    }
+    return render(request, 'geekshop/index.html', context=context)
 
 
 def contacts(request):
-    return render(request, 'contact.html')
+    title = 'контакты'
+    context = {
+        'title': title,
+        'links_menu': links_menu
+    }
+    return render(request, 'geekshop/contact.html', context=context)
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,21 @@
+import json
+
 from django.shortcuts import render
 
+with open('geekshop/templates/geekshop/links_menu.json', 'r', encoding="utf-8") as content:
+    links_menu = json.load(content)
+
+product_items = [
+    {'img': '/static/geekshop/img/product-11.jpg', 'title': 'Стул повышенного качества', 'caption': 'Не оторваться.'},
+    {'img': '/static/geekshop/img/product-21.jpg', 'title': 'Стул повышенного качества', 'caption': 'Не оторваться.'},
+    {'img': '/static/geekshop/img/product-31.jpg', 'title': 'Стул повышенного качества', 'caption': 'Не оторваться.'},
+]
 
 def products(request):
-    return render(request, 'products.html')
+    title = 'продукты'
+    context = {
+        'title': title,
+        'product_items': product_items,
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context=context)
