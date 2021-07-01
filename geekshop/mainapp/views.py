19605147958246db8f49acbcb1291b89dbd9fa79
Anton Lushnikov <antonlushnikow@gmail.com>
2021-07-01 22:41:54 +0500

    lesson4

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -9,11 +9,7 @@ with open('geekshop/templates/geekshop/links_menu.json', 'r', encoding="utf-8") 
 product_categories = ProductCategory.objects.all()
 
 product_items = Product.objects.all()
-# product_items = [
-#     {'img': '/static/geekshop/img/product-11.jpg', 'title': 'Стул повышенного качества', 'caption': 'Не оторваться.'},
-#     {'img': '/static/geekshop/img/product-21.jpg', 'title': 'Стул повышенного качества', 'caption': 'Не оторваться.'},
-#     {'img': '/static/geekshop/img/product-31.jpg', 'title': 'Стул повышенного качества', 'caption': 'Не оторваться.'},
-# ]
+
 
 def products(request):
     title = 'продукты'
