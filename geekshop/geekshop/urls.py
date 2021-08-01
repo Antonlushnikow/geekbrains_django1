ac6a0f7f1e6e064541a65087a83c8ad25f07a03e
Anton Lushnikov <antonlushnikow@gmail.com>
2021-08-01 09:14:30 +0500

    lesson2.3

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('basket/', include('basketapp.urls', namespace='basket')),
+    path('order/', include('ordersapp.urls', namespace='order')),
 
     path('', include('social_django.urls', namespace='social')),
 ]
