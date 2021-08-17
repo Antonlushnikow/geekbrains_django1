e8b2a70afcf94f16f29b36ae0cedccf8897de8fb
Anton Lushnikov <antonlushnikow@gmail.com>
2021-08-17 21:34:15 +0500

    lesson2.7 add cache in url

--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -5,12 +5,12 @@ from django.views.decorators.cache import cache_page
 app_name = 'products'
 
 urlpatterns = [
-    path('', ProductsListView.as_view(), name='index'),
-    path('category/<int:pk>/', ProductsListView.as_view(), name='category'),
+    path('', cache_page(3600)(ProductsListView.as_view()), name='index'),
+    path('category/<int:pk>/', cache_page(3600)(ProductsListView.as_view()), name='category'),
     # path('category/<int:pk>/', products, name='category'),
     # path('category/<int:pk>/page/<int:page>/', products, name='page'),
     path('product/<int:pk>/', cache_page(3600)(ProductReadView.as_view()), name='product'),
     # path('product/<int:pk>/', product, name='product'),
-    path('category/<int:pk>/ajax/', products_ajax),
+    path('category/<int:pk>/ajax/', cache_page(3600)(products_ajax)),
     # path('category/<int:pk>/page/<int:pk>/ajax/', cache_page(3600)(products_ajax)),
 ]
